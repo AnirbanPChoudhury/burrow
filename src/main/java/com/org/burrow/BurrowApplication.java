8fe822252c222f9e586b13d98fb2a09520068cef
nibirbora <nibirbora576@gmail.com>
2025-03-05 23:14:00 +0530

    Added println

--- src/main/java/com/org/burrow/BurrowApplication.java
+++ src/main/java/com/org/burrow/BurrowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BurrowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BurrowApplication.class, args);
+		System.out.println("Burrow Application Started");
 	}
 
 }
